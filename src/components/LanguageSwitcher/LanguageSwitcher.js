3f058074ac61ca92203697feca097ccbb7502643
nutsflag <26460023+nutsflag@users.noreply.github.com>
2019-04-07 15:05:56 +0200

    Add Fr to languageswitcher

--- src/components/LanguageSwitcher/LanguageSwitcher.js
+++ src/components/LanguageSwitcher/LanguageSwitcher.js
@@ -17,18 +17,52 @@ class LanguageSwitcher extends React.Component {
     this.props.i18n.changeLanguage(e.target.dataset.locale)
   }
 
-  render() {
-    return <div className={this.props.className}>
-      <p>
-        <span>{this.t('language')} | </span>
-        {
-          this.t('locale') === 'en'
-          ? <a href="?lng=zh" data-locale="zh" onClick={this.changeLanguage.bind(this)}>中文</a>
-          : <a href="?lng=en" data-locale="en" onClick={this.changeLanguage.bind(this)}>English</a>
-        }
-      </p>
-    </div>
-  }
-}
+  render()
+    {
+    	if (this.t('locale') === 'en') {
+    		return (
+        		<div className={this.props.className}> 
+        			<p>
+        				<span>{this.t('language')}
+        					<a> | </a>
+        					<a href="?lng=fr" data-locale="fr" onClick={this.changeLanguage.bind(this)}>Français</a>
+        					<a> | </a>
+        					<a href="?lng=zh" data-locale="zh" onClick={this.changeLanguage.bind(this)}>中文</a>
+        				</span>
+        			</p>
+        	    </div>
+        	);
+       	}
 
+        else if (this.t('locale') === 'fr') {
+    		return (
+        		<div className={this.props.className}> 
+        			<p>
+        				<span>{this.t('language')}
+        					<a> | </a>
+        					<a href="?lng=en" data-locale="en" onClick={this.changeLanguage.bind(this)}>English</a>
+        					<a> | </a>
+        					<a href="?lng=zh" data-locale="zh" onClick={this.changeLanguage.bind(this)}>中文</a>
+        				</span>
+        			</p>
+        	    </div>
+        	);
+        }
+
+        else if (this.t('locale') === 'zh') {
+    		return (
+        		<div className={this.props.className}> 
+        			<p>
+        				<span>{this.t('language')}
+        					<a> | </a>
+        					<a href="?lng=en" data-locale="en" onClick={this.changeLanguage.bind(this)}>English</a>
+        					<a> | </a>
+        					<a href="?lng=fr" data-locale="fr" onClick={this.changeLanguage.bind(this)}>Français</a>
+        				</span>
+        			</p>
+        	    </div>
+        	);
+        }
+    }
+}
 export default withTranslation()(LanguageSwitcher);
